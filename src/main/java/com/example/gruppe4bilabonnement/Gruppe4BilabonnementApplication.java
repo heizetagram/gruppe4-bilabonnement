943d2db38346bc4d5853a74c66830c0ff088a728
David Lu <davidlu1999@gmail.com>
2024-05-01 14:23:17 +0200

    Initial commit

---
+++ src/main/java/com/example/gruppe4bilabonnement/Gruppe4BilabonnementApplication.java
@@ -0,0 +1,13 @@
+package com.example.gruppe4bilabonnement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gruppe4BilabonnementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gruppe4BilabonnementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gruppe4bilabonnement/Gruppe4BilabonnementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gruppe4bilabonnement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gruppe4BilabonnementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
