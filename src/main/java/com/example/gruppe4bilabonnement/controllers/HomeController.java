3a1094617b24d26ad3787e27f06dffcba5cc356f
heizetagram <davidlu1999@gmail.com>
2024-05-05 22:13:52 +0200

    - Login functionality - Logout feature - Cannot log in using an email without '.'

--- src/main/java/com/example/gruppe4bilabonnement/controllers/HomeController.java
+++ src/main/java/com/example/gruppe4bilabonnement/controllers/HomeController.java
@@ -7,10 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -30,7 +27,22 @@ public class HomeController {
 
         Cookie cookie = new Cookie("employeeRole", employee.getEmployeeRole());
         response.addCookie(cookie);
-        return "redirect:/employee_frontpage";
+
+        // Returns an invalid login message if the user-login doesn't exist or if the email doesn't include a '.'
+        if (employee.getEmployeeRole().equals("N/A") || !email.contains(".")) {
+            return "home/login";
+        } else {
+            return "redirect:/employee_frontpage";
+        }
+    }
+
+    // Delete 'employeeRole' cookie and log out
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        Cookie cookie = new Cookie("employeeRole", null);
+        cookie.setMaxAge(0); // Tell browser to delete cookie
+        response.addCookie(cookie);
+        return "redirect:/";
     }
 
     @GetMapping("/employee_frontpage")
