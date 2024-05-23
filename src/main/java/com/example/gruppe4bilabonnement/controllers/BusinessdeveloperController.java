c3adffbf46c24550e5231ad50dd5d51c7eee01af
heizetagram <davidlu1999@gmail.com>
2024-05-24 00:30:20 +0200

    Added cookie value and RequestMapping to business_dev

--- src/main/java/com/example/gruppe4bilabonnement/controllers/BusinessdeveloperController.java
+++ src/main/java/com/example/gruppe4bilabonnement/controllers/BusinessdeveloperController.java
@@ -3,55 +3,83 @@ package com.example.gruppe4bilabonnement.controllers;
 import com.example.gruppe4bilabonnement.services.BusinessdeveloperService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
 @Controller
+@RequestMapping("/business_developer")
 public class BusinessdeveloperController {
 
     @Autowired
     private BusinessdeveloperService businessdeveloperService;
         // Gennemsnitlig betalingstid
     @GetMapping("/average_payment_time")
-    public String getAveragePaymentTime(Model model) {
-        double averagePaymentTime = businessdeveloperService.getAveragePaymentTime();
-        model.addAttribute("averagePaymentTime", averagePaymentTime);
-        return "businessdeveloper/average_payment_time";
+    public String getAveragePaymentTime(Model model, @CookieValue(name = "employeeRole") String cookieValue) {
+        if (cookieValue.equals("BUSINESS_DEV")) {
+            double averagePaymentTime = businessdeveloperService.getAveragePaymentTime();
+            model.addAttribute("averagePaymentTime", averagePaymentTime);
+            return "businessdeveloper/average_payment_time";
+        } else {
+            return "redirect:/";
+        }
     }
         // gennemsnitlig transporttid(hvordan fuck gør jeg lige det, gps or some shit, samme med co2 etc)
     @GetMapping("/average_transport_time")
-    public String getAverageTransportTime(Model model) {
-        double averageTransportTime = businessdeveloperService.getAverageTransportTime();
-        model.addAttribute("averageTransportTime", averageTransportTime);
-        return "businessdeveloper/average_transport_time";
+    public String getAverageTransportTime(Model model, @CookieValue(name = "employeeRole") String cookieValue) {
+        if (cookieValue.equals("BUSINESS_DEV")) {
+            double averageTransportTime = businessdeveloperService.getAverageTransportTime();
+            model.addAttribute("averageTransportTime", averageTransportTime);
+            return "businessdeveloper/average_transport_time";
+        } else {
+            return "redirect:/";
+        }
     }
         //gennemsnitlig Leasetid
 
     @GetMapping("/leasing_speed")
-    public String getLeasingSpeedForCar(@RequestParam long carId, Model model) {
-        double leasingSpeed = businessdeveloperService.getLeasingSpeedForCar(carId);
-        model.addAttribute("carId", carId);
-        model.addAttribute("leasingSpeed", leasingSpeed);
-        return "businessdeveloper/leasing_speed";
+    public String getLeasingSpeedForCar(@RequestParam long carId, Model model, @CookieValue(name = "employeeRole") String cookieValue) {
+        if (cookieValue.equals("BUSINESS_DEV")) {
+            double leasingSpeed = businessdeveloperService.getLeasingSpeedForCar(carId);
+            model.addAttribute("carId", carId);
+            model.addAttribute("leasingSpeed", leasingSpeed);
+            return "businessdeveloper/leasing_speed";
+        } else {
+            return "redirect:/";
+        }
     }
 
     //totale antal biler udlejet på givent tidspunkt
     @GetMapping("/rented_cars_count")
-    public String getRentedCarsCount(Model model) {
-        int rentedCarsCount = businessdeveloperService.getNumberOfRentedCars();
-        model.addAttribute("rentedCarsCount", rentedCarsCount);
-        return "businessdeveloper/rented_cars_count";
+    public String getRentedCarsCount(Model model, @CookieValue(name = "employeeRole") String cookieValue) {
+        if (cookieValue.equals("BUSINESS_DEV")) {
+            int rentedCarsCount = businessdeveloperService.getNumberOfRentedCars();
+            model.addAttribute("rentedCarsCount", rentedCarsCount);
+            return "businessdeveloper/rented_cars_count";
+        } else {
+            return "redirect:/";
+        }
     }
         // Pris på udlejede biler på givent tidspunkt
     @GetMapping("/total_price_of_rented_cars")
-    public String getTotalPriceOfRentedCars(Model model) {
-        double totalPrice = businessdeveloperService.getTotalPriceOfRentedCars();
-        model.addAttribute("totalPrice", totalPrice);
-        return "businessdeveloper/total_price_of_rented_cars";
+    public String getTotalPriceOfRentedCars(Model model, @CookieValue(name = "employeeRole") String cookieValue) {
+        if (cookieValue.equals("BUSINESS_DEV")) {
+            double totalPrice = businessdeveloperService.getTotalPriceOfRentedCars();
+            model.addAttribute("totalPrice", totalPrice);
+            return "businessdeveloper/total_price_of_rented_cars";
+        } else {
+            return "redirect:/";
+        }
     }
+
     @GetMapping("/business_developer_dashboard")
-    public String showBusinessDeveloperDashboard() {
-        return "businessdeveloper/business_developer_dashboard";
+    public String showBusinessDeveloperDashboard(@CookieValue(name = "employeeRole") String cookieValue) {
+        if (cookieValue.equals("BUSINESS_DEV")) {
+            return "businessdeveloper/business_developer_dashboard";
+        } else {
+            return "redirect:/";
+        }
     }
 }
